a34db89b8f2c680bfa43eaac0651d257d685b850
nikoladavitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2021-11-07 15:14:31 +0100

    Add files via upload

---
+++ sedc-server/sedc-server-try-two/NiceConsoleLogger.cs
@@ -0,0 +1,41 @@
+﻿using sedc_server_Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sedc_server_try_two
+{
+    class NiceConsoleLogger : ILogger
+    {
+        public LogLevel Level { get; set; }
+
+        public void Log(string message, LogLevel level = LogLevel.Information)
+        {
+            if(level < Level)
+            {
+                return;
+            }
+            if(level < LogLevel.Error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            if(level < LogLevel.Debug)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            if(level < LogLevel.Information)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if(level < LogLevel.Warning)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+    }
+}
