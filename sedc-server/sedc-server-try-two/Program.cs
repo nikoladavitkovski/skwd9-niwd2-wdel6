1e72bc452f22f6fb5a5b2773b0713db27426ccfa
nikoladavitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2021-11-10 11:04:12 +0100

    Add files via upload

--- sedc-server/sedc-server-try-two/Program.cs
+++ sedc-server/sedc-server-try-two/Program.cs
@@ -1,12 +1,14 @@
-﻿using Sedc.Server.Core;
-
+﻿using Azure.Core;
+using Sedc.Server.Core;
+using sedc_server_Server;
 using System;
+using Request = Sedc.Server.Core.Request;
 
 namespace sedc_server_try_two
 {
     class MyRequestProcessor : IRequestProcessor
     {
-        public IResponse Process(Request request)
+        public IResponse Process(Request request, ILogger logger)
         {
             return new TextResponse
             {
@@ -22,19 +24,35 @@ namespace sedc_server_try_two
                 Status = Status.OK
             };
         }
+
+        public IResponse Process(Azure.Core.Request request, ILogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldProcess(Request request)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            var logger = new CompositeLogger();
+            logger.AddLogger(new NiceConsoleLogger { Level = LogLevel.Debug });
+            logger.AddLogger(new FileLogger("log.txt"));
+
             try
             {
                 Server server = new Server(new ServerOptions
                 {
                     Port = 668,
-                    Processor = new FileRequestProcessor(@"public-sedc")
+                    Logger = logger
                 });
+                server.RegisterProcessor(new FileRequestProcessor("public-sedc"));
+                server.RegisterProcessor(new ApiRequestProcessor());
                 server.Start();
             }
             catch (ApplicationException aex)
