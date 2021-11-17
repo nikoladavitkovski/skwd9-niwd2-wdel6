80bc1c4c9f7546badee5a1dbc9c937cce4865a99
nikoladavitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2021-11-17 11:15:19 +0100

    Add files via upload

---
+++ sedc-server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            Type type = new Type("Poem");
+            type.GetType();
+        }
+        class Books
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+
+            public string Author { get; set; }
+
+            public int BookId { get; set; }
+        }
+        class Type : Books
+        {
+            public Type(string genre)
+            {
+                Genre = genre;
+            }
+            public string Genre { get => base.Title; set => base.Title = value; }
+        }
+    }
+}
